9acdf1419f41815d0f8faeffe51537f187bc46d6
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2024-02-15 12:28:53 -0300

    Aula 01

---
+++ AULA 01/Aula01_POO/Aula01_POO/Conta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01_POO
+{
+    public class Conta
+    {
+        public double Saldo { get; set; }
+        public double Limite { get; set; }
+        public int Numero { get; set; }
+    }
+}
+
+
+
+
---
+++ AULA 01/Aula01_POO/Aula01_POO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula01_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aula 01 - POO");
+            
+            //Criando uma instância da classe Conta
+            Conta conta1 = new Conta();
+
+            //Atribuindo os valores para cada atributo da classe Conta
+            conta1.Saldo = 1500;
+            conta1.Limite = 500;
+            conta1.Numero = 123;
+
+            Console.WriteLine("Olá fulano, seu saldo em conta é de: R$ " + conta1.Saldo);
+            Console.WriteLine("Seu limite de saque é de R$ " + conta1.Limite);
+            Console.WriteLine("Sua conta é nº " + conta1.Numero);
+
+
+        }
+    }
+}
+
+
+
