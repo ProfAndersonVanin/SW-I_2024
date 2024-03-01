2b031b70e4371f8ee508738b84f433ebd5fb02f4
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2024-03-01 10:55:57 -0300

    Arquivos Iniciais da modelagem da Lampada

---
+++ AULA 01/Aula01_POO/ProjetoLampada/ProjetoLampada/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjetoLampada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampada lamp1 = new Lampada();
+
+            lamp1.Ligada = true;
+            lamp1.Potencia = 100;
+            lamp1.Cor = "Vermelho";
+
+            Console.WriteLine("Mostrando Dados do Objeto lamp1");
+            Console.WriteLine("A lâmpada está ligada? " + lamp1.Ligada);
+            Console.WriteLine("A potência da lâmpada é: " + lamp1.Potencia);
+            Console.WriteLine("A cor da lâmpada é: " + lamp1.Cor);
+
+        }
+    }
+}
